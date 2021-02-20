e1757d643fe14296337855251458d9f929c25f46
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-20 08:27:25 +0300

    Initial commit

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,49 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Consumer {
+  Map<Integer, Deque<String>> tasks = new HashMap<>();
+  LinkedList<Integer> priorities = new LinkedList<>();
+
+  public Consumer() {
+    this.priorities.add(0);
+    this.priorities.add(1);
+    this.priorities.add(2);
+  }
+
+  public void addTask (String task, int priority) {
+    if (this.tasks.containsKey(priority)) {
+      Deque<String> tasksByPriority = this.tasks.get(priority);
+      tasksByPriority.add(task);
+      this.tasks.put(priority, tasksByPriority);
+    } else {
+      if (!this.priorities.contains(priority)) {
+        throw new IllegalArgumentException("Кривой приоритет");
+      }
+      Deque<String> tasksByPriority = new LinkedList<>();
+      tasksByPriority.add(task);
+      this.tasks.put(priority, tasksByPriority);
+    }
+  }
+
+
+  public String getTask () {
+    for (int currentPriority : this.priorities) {
+      if (this.tasks.containsKey(currentPriority) && !this.tasks.get(currentPriority).isEmpty()) {
+        return this.tasks.get(currentPriority).pollFirst();
+      }
+    }
+    return null;
+  }
+}
+
+
+// 2 метода: addTask, getTask
+// add - 1 параметр задача (стринг), 2 параметр - важность
+// get - вернуть задачу, добавленную раньше других, но не забыть про важность
+
+// addPriority - добавить. Положительный int. Больше приоритет -> важнее задача
+// добавить TestNG, собрать maven
+// написать тесты для консьюмера
