f595ddd9d1882754ec4b0ff87a4fc28ff7647de6
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-20 22:26:28 +0300

    Добавил микротесты

---
+++ src/test/java/ConsumerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConsumerTest {
+  private Consumer consumer = new Consumer();
+
+  @Test
+  public void testConsumer() {
+    consumer.addTask("Task1", 1);
+    consumer.addTask("Task2", 0);
+    consumer.addTask("Task3", 3);
+    consumer.addTask("Task4", 2);
+
+    Assert.assertEquals("Task3", consumer.getTask());
+    Assert.assertEquals("Task4", consumer.getTask());
+    Assert.assertEquals("Task1", consumer.getTask());
+    Assert.assertEquals("Task2", consumer.getTask());
+    Assert.assertEquals(null, consumer.getTask());
+  }
+}
