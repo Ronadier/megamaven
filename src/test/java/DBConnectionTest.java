a1e0b3a7100b0548c3276f2cb583cee81b67b507
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-02-23 09:18:07 +0300

    DBConnectionTest

--- src/test/java/DBConnectionTest.java
+++ src/test/java/DBConnectionTest.java
@@ -1,10 +1,30 @@
 import org.testng.annotations.Test;
 
+import java.sql.*;
+
 public class DBConnectionTest {
 
   @Test
   public void testDBConnect () {
 
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://109.196.164.32:3306/learnqa?user=learnqa&password=learnqa");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select * from Cats");
+      while (rs.next()) {
+        System.out.println("Cat's id: " + rs.getString(1) + " with name: " + rs.getString(2) + ", birthday: " + rs.getString(3));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      // Do something with the Connection
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
 
   }
 }
